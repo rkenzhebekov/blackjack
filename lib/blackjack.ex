aafc60c8a491cbd5e9f6ad2fea7fcdaf7cc9863a
Ruslan Kenzhebekov <reke.essential@gmail.com>
2022-05-12 20:10:13 -0700

    Initial project structure

---
+++ lib/blackjack.ex
@@ -0,0 +1,18 @@
+defmodule Blackjack do
+  @moduledoc """
+  Documentation for `Blackjack`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Blackjack.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
